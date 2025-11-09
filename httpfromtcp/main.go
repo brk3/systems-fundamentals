ffb32bc09abe152cdce5a93d22bea9b490de6e6c
Paul Bourke <pauldbourke@protonmail.com>
2025-11-09 22:12:22 +0000

    ch1 - l5 channel refactor

--- httpfromtcp/main.go
+++ httpfromtcp/main.go
@@ -2,10 +2,43 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"strings"
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	c := make(chan string)
+
+	out := make([]byte, 8)
+	currentLine := ""
+
+	go func(c chan string) {
+		for {
+			// read 8b
+			n, err := f.Read(out)
+			if err != nil {
+				break
+			}
+
+			parts := strings.Split(string(out[:n]), "\n")
+
+			if len(parts) == 1 {
+				currentLine += parts[0]
+			} else {
+				for i := 0; i < len(parts)-1; i++ {
+					currentLine += parts[i]
+				}
+				c <- currentLine
+				currentLine = parts[len(parts)-1]
+			}
+		}
+		close(c)
+	}(c)
+
+	return c
+}
 
 func main() {
 	f, err := os.OpenFile("messages.txt", os.O_RDWR|os.O_CREATE, 0644)
@@ -13,27 +46,7 @@ func main() {
 		log.Fatal(err)
 	}
 	defer f.Close()
-
-	out := make([]byte, 8)
-	currentLine := ""
-
-	for {
-		// read 8b
-		n, err := f.Read(out)
-		if err != nil {
-			break
-		}
-
-		parts := strings.Split(string(out[:n]), "\n")
-
-		if len(parts) == 1 {
-			currentLine += parts[0]
-		} else {
-			for i := 0; i < len(parts)-1; i++ {
-				currentLine += parts[i]
-			}
-			fmt.Printf("read: %s\n", currentLine)
-			currentLine = parts[len(parts)-1]
-		}
+	for s := range getLinesChannel(f) {
+		fmt.Printf("read: %s\n", s)
 	}
 }
