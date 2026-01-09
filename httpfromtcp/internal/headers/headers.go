78bef45c90444f768cfa008d4cc30d41a9f3e175
Paul Bourke <pauldbourke@protonmail.com>
2026-01-09 16:12:28 +0000

    ch5 - l2 - header key constraints

--- httpfromtcp/internal/headers/headers.go
+++ httpfromtcp/internal/headers/headers.go
@@ -25,11 +25,33 @@ func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 		return 0, false, fmt.Errorf("invalid header '%s'", rawHeader)
 	}
 
+	if !validFieldName(key) {
+		return 0, false, fmt.Errorf("invalid header '%s'", rawHeader)
+	}
+
 	// check for white space before key or after colon
 	if key[0] == ' ' || key[len(key)-1] == ' ' {
 		return 0, false, fmt.Errorf("invalid header '%s'", rawHeader)
 	}
 
-	h[strings.TrimSpace(string(key))] = strings.TrimSpace(string(value))
+	h[strings.ToLower(strings.TrimSpace(string(key)))] = strings.TrimSpace(string(value))
 	return clrfIndex + 2, false, nil
+}
+
+func validFieldName(name []byte) bool {
+	if len(name) < 1 {
+		return false
+	}
+	for _, b := range name {
+		if (b >= 'a' && b <= 'z') || (b >= 'A' && b <= 'Z') || (b >= '0' && b <= '9') {
+			continue
+		} else {
+			switch b {
+			case '!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~':
+				continue
+			}
+		}
+		return false
+	}
+	return true
 }
--- httpfromtcp/internal/headers/headers_test.go
+++ httpfromtcp/internal/headers/headers_test.go
@@ -14,7 +14,7 @@ func TestHeaderParse(t *testing.T) {
 	n, done, err := headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "localhost:42069", headers["host"])
 	assert.Equal(t, 23, n)
 	assert.False(t, done)
 
@@ -32,11 +32,27 @@ func TestHeaderParse(t *testing.T) {
 	n, done, err = headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "localhost:42069", headers["host"])
 	assert.Equal(t, 28, n)
 	assert.False(t, done)
 
 	// TODO: assume implemented once we integate headers.Parse with our state machine
 	// Test: Valid 2 headers with existing headers
 	// Test: Valid done
+
+	// Test: Header keys should be case insensitive
+	headers = Headers{}
+	data = []byte("HOST: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["host"])
+
+	// Test: Header key with invalid char
+	headers = Headers{}
+	data = []byte("H©st: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
 }
