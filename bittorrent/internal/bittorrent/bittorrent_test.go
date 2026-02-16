67e600ac03e8370bc5cd0f62a3cd3b4538f17f58
Paul Bourke <pauldbourke@protonmail.com>
2026-02-16 15:21:19 +0000

    Add basic test for toTorrentFile

---
+++ bittorrent/internal/bittorrent/bittorrent_test.go
@@ -0,0 +1,34 @@
+package bittorrent
+
+import (
+	"bufio"
+	"os"
+	"testing"
+
+	bencode "github.com/jackpal/bencode-go"
+)
+
+func TestToTorrentFile(t *testing.T) {
+	f, err := os.Open("../../debian-11.5.0-amd64-netinst.iso.torrent")
+	if err != nil {
+		t.Errorf("error opening torrent file: %v", err)
+	}
+	defer f.Close()
+	r := bufio.NewReader(f)
+
+	b := bencodeTorrent{}
+	err = bencode.Unmarshal(r, &b)
+	if err != nil {
+		t.Errorf("error unmarshalling torrent file: %v", err)
+	}
+
+	tf, err := b.toTorrentFile()
+	if err != nil {
+		t.Errorf("error converting bencodeTorrent to TorrentFile file: %v", err)
+	}
+	want := 1528
+	have := len(tf.PieceHashes)
+	if want != have {
+		t.Errorf("expected %d piece hashes, got %d", want, have)
+	}
+}
